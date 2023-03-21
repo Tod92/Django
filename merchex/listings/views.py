cd3bcb77817d767024bc78983cd02ac4d69e73ac
Thomas Deruere <t.deruere@gmail.com>
2023-03-21 15:30:37 +0100

    first commit

---
+++ merchex/listings/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def hello(request):
+    return HttpResponse('<h1>Hello Django!</h1>')
+
+def about(request):
+    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>\
+                        <h2>La petite vie de Marie !</h2>')
+
+def listings(request):
+    return HttpResponse('<h1>Listings</h1> <p>ici une liste</p>')
+
+def contact(request):
+    return HttpResponse('<h1>Nous contacter</h1> <p>voici mon 06</p>')
