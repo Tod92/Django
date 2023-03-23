435096b105dd77c2b4253ad7e013e988c7a7667b
Thomas Deruere <t.deruere@gmail.com>
2023-03-23 11:24:25 +0100

    3-1 terminé; definitions types et valeurs par defaut sur modèles et migrations

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,8 +1,37 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Band(models.Model):
+
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+        ROCK = 'RO'
+        ELECTRO = 'EL'
+        DRUM_AND_BASS = 'DB'
+
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(max_length=5,choices=Genre.choices)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(validators=[MinValueValidator(1900), MaxValueValidator(2021)])
+    active = models.fields.BooleanField(default=True)
+    #  Nous pouvons utiliser l'option null=True pour autoriser les valeurs NULL dans la base de données. Et lorsque
+    #  nous créerons un formulaire pour créer ou modifier des objets Band , le fait de définir blank=True ici nous
+    #  permettra de soumettre ce formulaire avec une zone de texte vide pour ce champ.
+    official_homepage = models.fields.URLField(null=True, blank=True)
 
 class Listing(models.Model):
+
+    class Type(models.TextChoices):
+        RECORDS = 'RE'
+        CLOTHINGS = 'CL'
+        POSTERS = 'PO'
+        MISCELLANEOUS = 'MI'
+
     title = models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=1000)
+    sold = models.fields.BooleanField(default=False)
+    year = models.fields.IntegerField(null=True)
+    type = models.fields.CharField(max_length=5,choices=Type.choices)
